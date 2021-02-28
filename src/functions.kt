bd73171ab578db9d064e9829e8436c439bf8bebd
Marie-Louise <mariemuteteyimana@gmail.com>
2021-02-28 21:35:33 +0300

    first commit

---
+++ src/functions.kt
@@ -0,0 +1,34 @@
+fun main() {
+    println("Hello marie louise")
+    var result=modulus(3,2)
+    println(result)
+    println("sociable")
+    addition()
+
+}
+
+fun name() {
+    val name="Hello marie louise"
+    println("Hello marie louise")
+}
+fun modulus(num1:Int,num2:Int) :Int{
+    var num1=3
+    var num2=2
+    var modulus=(num1%num2)
+    return modulus
+
+}
+
+fun interestingFact() {
+    var interestingFact = "sociable"
+    println(interestingFact)
+}
+fun addition(){
+    var num1=10
+    var num2=20
+    var num3=30
+    var num4=40
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+
